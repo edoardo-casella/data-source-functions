6352925008a3d73570e324fe2fb7fc564502fa4c
edoardo-casella <edoardo.casella@gmail.com>
2026-02-12 18:04:53 +0100

    Update Contacts.cs

--- FunctionApp/Dynamics365/CRM/Contacts.cs
+++ FunctionApp/Dynamics365/CRM/Contacts.cs
@@ -6,46 +6,103 @@ using System.Text.Json.Nodes;
 
 namespace Plumsail.DataSource.Dynamics365.CRM
 {
-    public class Contacts(HttpClientProvider httpClientProvider, ILogger<Contacts> logger)
+    public class Eventi(HttpClientProvider httpClientProvider, ILogger<Eventi> logger)
     {
-        [Function("D365-CRM-Contacts")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "crm/contacts/{id?}")] HttpRequest req, Guid? id)
+        [Function("D365-CRM-Eventi")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "crm/eventi/{id?}")] HttpRequest req, 
+            Guid? id)
         {
-            logger.LogInformation("Dynamics365-CRM-Contacts is requested.");
-
+            logger.LogInformation("Dynamics365-CRM-Eventi is requested.");
+            
             try
             {
                 var client = httpClientProvider.Create();
-
+                
                 if (!id.HasValue)
                 {
-                    var contactsJson = await client.GetStringAsync("contacts?$select=contactid,fullname");
-                    var contacts = JsonValue.Parse(contactsJson);
-                    return new OkObjectResult(contacts?["value"]);
+                    // Query tutti i Calendar Venue con expand su Evento
+                    var query = "cr6ef_calendariovenues" +
+                        "?$filter=cr6ef_tipologia eq 848780001" +
+                        "&$expand=cr6ef_evento($select=cr6ef_eventoid,cr6ef_nomeevento,cr6ef_status)" +
+                        "&$select=cr6ef_calendariovenueid,cr6ef_inizio,cr6ef_venue,cr6ef_tipologia";
+                    
+                    var calendarioVenueJson = await client.GetStringAsync(query);
+                    var calendarioVenue = JsonValue.Parse(calendarioVenueJson);
+                    
+                    // Trasforma i dati appiattendo la struttura
+                    var value = calendarioVenue?["value"]?.AsArray();
+                    if (value != null)
+                    {
+                        var transformed = new JsonArray();
+                        
+                        foreach (var item in value)
+                        {
+                            var evento = item?["cr6ef_evento"];
+                            
+                            // Salta se evento non presente
+                            if (evento == null) continue;
+                            
+                            var transformedItem = new JsonObject
+                            {
+                                ["calendariovenueid"] = item?["cr6ef_calendariovenueid"],
+                                ["dataEvento"] = item?["cr6ef_inizio"],
+                                ["venue"] = item?["cr6ef_venue"],
+                                ["tipologia"] = item?["cr6ef_tipologia"],
+                                ["eventoId"] = evento?["cr6ef_eventoid"],
+                                ["nomeEvento"] = evento?["cr6ef_nomeevento"],
+                                ["status"] = evento?["cr6ef_status"]
+                            };
+                            
+                            transformed.Add(transformedItem);
+                        }
+                        
+                        return new OkObjectResult(transformed);
+                    }
+                    
+                    return new OkObjectResult(new JsonArray());
                 }
-
-                var contactResponse = await client.GetAsync($"contacts({id})");
-                if (!contactResponse.IsSuccessStatusCode)
+                
+                // Query singolo calendar venue per ID
+                var singleQuery = $"cr6ef_calendariovenues({id})" +
+                    "?$expand=cr6ef_evento($select=cr6ef_eventoid,cr6ef_nomeevento,cr6ef_status)" +
+                    "&$select=cr6ef_calendariovenueid,cr6ef_inizio,cr6ef_venue,cr6ef_tipologia";
+                
+                var calendarioResponse = await client.GetAsync(singleQuery);
+                
+                if (!calendarioResponse.IsSuccessStatusCode)
                 {
-                    if (contactResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    if (calendarioResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
                     {
                         return new NotFoundResult();
                     }
-
-                    // throws Exception
-                    contactResponse.EnsureSuccessStatusCode();
+                    calendarioResponse.EnsureSuccessStatusCode();
                 }
-
-                var contactJson = await contactResponse.Content.ReadAsStringAsync();
+                
+                var calendarioJson = await calendarioResponse.Content.ReadAsStringAsync();
+                var calendario = JsonValue.Parse(calendarioJson);
+                var eventoData = calendario?["cr6ef_evento"];
+                
+                var transformedSingle = new JsonObject
+                {
+                    ["calendariovenueid"] = calendario?["cr6ef_calendariovenueid"],
+                    ["dataEvento"] = calendario?["cr6ef_inizio"],
+                    ["venue"] = calendario?["cr6ef_venue"],
+                    ["tipologia"] = calendario?["cr6ef_tipologia"],
+                    ["eventoId"] = eventoData?["cr6ef_eventoid"],
+                    ["nomeEvento"] = eventoData?["cr6ef_nomeevento"],
+                    ["status"] = eventoData?["cr6ef_status"]
+                };
+                
                 return new ContentResult()
                 {
-                    Content = contactJson,
+                    Content = transformedSingle.ToJsonString(),
                     ContentType = "application/json"
                 };
             }
             catch (HttpRequestException ex)
             {
-                logger.LogError(ex, "An error has occured while processing Dynamics365-CRM-Contacts request.");
+                logger.LogError(ex, "An error has occurred while processing Dynamics365-CRM-Eventi request.");
                 return new StatusCodeResult(ex.StatusCode.HasValue ? (int)ex.StatusCode.Value : StatusCodes.Status500InternalServerError);
             }
         }
